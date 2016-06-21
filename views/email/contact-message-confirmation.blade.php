847822a099bf1d26987b6a9706ae4c061369c274
2osha <ahmedroshdi2014@gmail.com>
2016-06-21 05:19:12 -0700

    Add files via upload

---
+++ views/email/contact-message-confirmation.blade.php
@@ -0,0 +1,8 @@
+<h1>We Recieved Your Email !!</h1>
+
+<p>Thank you , we will respose as soon as possible</p>
+
+<h3>Information about your message </h3>
+
+<p>Subject :: {{ $contact_message->subject }}</p>
+
---
+++ views/email/contact-message-notification.blade.php
@@ -0,0 +1,8 @@
+<h1>A new Contact Message was sent !!</h1>
+
+<h3>Information about your message </h3>
+
+<p>Subject :: {{ $contact_message->subject }}</p>
+
+<p>Message :: {{ $contact_message->body }}</p>
+
